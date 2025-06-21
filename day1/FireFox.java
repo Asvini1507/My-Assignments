8ac67b0a998526c929ff45e0ea5bd0916dc6a761
Asvini1507 <kasvinibe1992@gmail.com>
2025-06-22 00:46:48 +0530

    Add files via upload
    Week1-day1

---
+++ day1/FireFox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) 
+	{
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releeseYear = 1998;
+		char browserLogo = 'f';
+				System.out.println("browserVersion:" + browserVersion);
+				System.out.println("browserName:" +browserName);
+				System.out.println("isVisible:" +isVisible);
+				System.out.println("releeseYear:" +releeseYear);
+				System.out.println("browserLogo:" +browserLogo);
+	}
+
+}
