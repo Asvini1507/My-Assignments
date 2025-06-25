bfd3127c8beb62feacc9bc56b3e4d9bfd29e8263
Asvini1507 <kasvinibe1992@gmail.com>
2025-06-26 01:10:39 +0530

    Add files via upload
    week1.day2

---
+++ day2/Fibonacciseries.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class Fibonacciseries {
+
+	public static void main(String[] args)
+	{
+		int n=10;
+		int n1=0;
+		int n2=1;
+		System.out.println("fibonacci series");
+		for(int i=1;i<=n;i++)
+		{ 
+			System.out.println(n1+ "");
+			int n3=n1+n2;
+			n1=n2;
+			n2=n3;
+	
+		}
+			
+		
+
+	}
+
+}
---
+++ day2/Oddevencheck.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+public class Oddevencheck {
+
+	public static void main(String[] args) 
+	{
+	  int number=35;
+	  if(number%2==0) 
+	  {
+		System.out.println("the given number is even");  
+	  }
+	  else
+	  {
+		  System.out.println("the given number is odd");
+	  }
+	}
+
+}
